64b70f13875e9e0e681ac4ffebe7c1645559403a
nikita <Niktaromanof@yandex.ru>
2023-04-02 03:53:39 +0700

    t1

--- MergedLinkedList.java
+++ MergedLinkedList.java
@@ -3,14 +3,6 @@
      Merge the two lists in a one sorted list.
      The list should be made by splicing together the nodes of the first two lists.
      Return the head of the merged linked list.
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode() {}
-     *     ListNode(int val) { this.val = val; }
-     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
      */
 
 public class MergedLinkedList {
