5707cd2d20b8276d1fb0b6c7abe5edbde469d2e0
Nikita Romanovskiy <Nikitaromanof@yandex.ru>
2023-05-03 12:20:49 +0700

    Update MergeKSortedLists.java

--- MergeKSortedLists.java
+++ MergeKSortedLists.java
@@ -1,4 +1,5 @@
 /*
+*HARD
 * You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 Merge all the linked-lists into one sorted linked-list and return it.
 */
